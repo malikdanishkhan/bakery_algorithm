d6c9b63ef063a7d0a3bbf707cf0d5770a59ecca6
DanishMalik <malik083.morris.umn.edu>
2018-04-26 13:21:47 -0500

    Initial commit

---
+++ lib/bakery_algorithm.ex
@@ -0,0 +1,18 @@
+defmodule BakeryAlgorithm do
+  @moduledoc """
+  Documentation for BakeryAlgorithm.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BakeryAlgorithm.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bakery_algorithm_test.exs
@@ -0,0 +1,8 @@
+defmodule BakeryAlgorithmTest do
+  use ExUnit.Case
+  doctest BakeryAlgorithm
+
+  test "greets the world" do
+    assert BakeryAlgorithm.hello() == :world
+  end
+end
