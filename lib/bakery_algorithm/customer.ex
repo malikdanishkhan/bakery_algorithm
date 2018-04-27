ee483f26791a7624940cb9c3444d074dc0ce5931
DanishMalik <malik083.morris.umn.edu>
2018-04-26 21:44:02 -0500

    Wake up customers after random period of time

--- lib/bakery_algorithm/customer.ex
+++ lib/bakery_algorithm/customer.ex
@@ -1,16 +1,23 @@
-defmodule Bakery do
-	def start(num_pid) when num_pid > 0 do
-              newpid = spawn(__MODULE__, :wake_up, [])
-	      send(newpid, {:ok, self()})
-	      IO.puts " Customer PID formed #{inspect newpid}"
-	      start(num_pid-1)
+defmodule Manager do
+	def start(num_customers) do
+	create_customers(num_customers)
+	end
+
+
+	defp create_customers(num_customers) when num_customers > 0 do
+              newcustpid = spawn(__MODULE__, :wake_up, [])
+	      send(newcustpid, {:ok, self()})
+	      IO.puts " Customer PID formed #{inspect newcustpid}"
+	      create_customers(num_customers-1)
 	  end
 
-	
+             	
 	def wake_up do
 		IO.puts "#{inspect self} is listening"
 	        receive do
 			{:ok, sender} ->
+			sleep = :rand.uniform(10000)
+			:timer.sleep(sleep)
 			IO.puts "[#{inspect sender}] A new customer has just arrived #{inspect self()}"
 		end
 	end
