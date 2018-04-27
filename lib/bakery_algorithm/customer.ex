a876203bc739595829876903de66e5eca44d081b
DanishMalik <malik083.morris.umn.edu>
2018-04-26 22:13:22 -0500

    Sleep time for customers fixed

--- lib/bakery_algorithm/customer.ex
+++ lib/bakery_algorithm/customer.ex
@@ -1,25 +1,27 @@
 defmodule Manager do
 	def start(num_customers) do
-	create_customers(num_customers)
+	Customer.create_customers(num_customers)
 	end
+end
 
-
-	defp create_customers(num_customers) when num_customers > 0 do
+defmodule Customer do
+	def create_customers(num_customers) when num_customers > 0 do
               newcustpid = spawn(__MODULE__, :wake_up, [])
-	      send(newcustpid, {:ok, self()})
+	      send(newcustpid, {:incoming, self()})
 	      IO.puts " Customer PID formed #{inspect newcustpid}"
 	      create_customers(num_customers-1)
 	  end
 
              	
 	def wake_up do
-		IO.puts "#{inspect self} is listening"
+	     IO.puts "#{inspect self} is listening"
 	        receive do
-			{:ok, sender} ->
+			{:incoming, sender} ->
 			sleep = :rand.uniform(10000)
 			:timer.sleep(sleep)
 			IO.puts "[#{inspect sender}] A new customer has just arrived #{inspect self()}"
 		end
 	end
 end
-			
+
+		
