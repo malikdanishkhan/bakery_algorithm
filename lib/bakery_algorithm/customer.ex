9bf33a113849a297218c6cda602f69d269430794
DanishMalik <malik083.morris.umn.edu>
2018-04-26 23:23:43 -0500

    Added server module that to create a list of servers

--- lib/bakery_algorithm/customer.ex
+++ lib/bakery_algorithm/customer.ex
@@ -1,16 +1,20 @@
-defmodule Manager do
-	def start(num_customers) do
+defmodule Main do
+	def start(num_customers, num_servers) do
+
+	Server.create_servers(num_servers)
 	Customer.create_customers(num_customers)
 	end
 end
 
 defmodule Customer do
-	def create_customers(num_customers) when num_customers > 0 do
+	def create_customers(num_customers) when num_customers >= 0 do
               newcustpid = spawn(__MODULE__, :wake_up, [])
 	      send(newcustpid, {:incoming, self()})
 	      IO.puts " Customer PID formed #{inspect newcustpid}"
 	      create_customers(num_customers-1)
-	  end
+        end
+	def create_customers(num_customers) do
+	end
 
              	
 	def wake_up do
@@ -24,4 +28,28 @@ defmodule Customer do
 	end
 end
 
-		
+defmodule Server do
+	def create_servers(num_servers) when num_servers >= 0 do
+              newserverpid = spawn(__MODULE__, :something, [])
+	      send(newserverpid, {:serverCreated, self()})
+	      IO.puts " Server formed #{inspect newserverpid}"
+	      create_servers(num_servers-1)
+	end
+	def create_servers(num_servers) do
+	end
+
+	def something do
+               receive do
+	       {:serverCreated, newserverpid} -> IO.puts"New server"
+	       end
+        end
+end
+
+defmodule Manager do
+    def match(servers) do
+	receive do
+		{:addserver, newserverpid} -> servers = servers ++ [newserverpid]
+        end
+    end
+end
+			
