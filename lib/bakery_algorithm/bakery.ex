c8a4899a7ad8b5257ec536e78aa2c53e9fdc90d4
DanishMalik <malik083.morris.umn.edu>
2018-04-28 18:49:19 -0500

    Added customer module (customer pids are working)

--- lib/bakery_algorithm/bakery.ex
+++ lib/bakery_algorithm/bakery.ex
@@ -1,9 +1,10 @@
 defmodule Bakery do
-	def start(num_s) do
+	def start(num_c, num_s) do
 	serverlist = []
 	manager = spawn(Manager, :manage, [serverlist])
 	Process.register(manager, :manager)
 	Server.create_servers(num_s)
+	Customer.create_customers(num_c)
 	end
 end
 
@@ -16,7 +17,7 @@ defmodule Server do
 	
 	def add_servers(servers, num_s, index) when num_s > 0 do 
 	firstserver = Enum.at(servers, index)
-	IO.puts"Adding server #{inspect firstserver} to Server List"
+	#IO.puts"Adding server #{inspect firstserver} to Server List"
 	send(:manager, {:addserver, firstserver})
 	add_servers(servers,num_s-1, index+1)
   	 
@@ -29,13 +30,30 @@ defmodule Server do
 	end
 	 
 end
-		
+
+defmodule Customer do
+	def create_customers(num_c) do	
+	#Creating customer PIDS
+	customers = (1..num_c) |> Enum.map(fn (n) -> spawn(__MODULE__, :somethingelse, []) end)
+	IO.puts"New cust PID #{inspect customers}"	
+	#Putting customers to sleep for random time (to simulate customers arriving at different times)
+	goodnight(customers)
+	end
+
+	def goodnight(customers) do
+	IO.puts"Customers are here to sleep #{inspect customers}"
+	end
+
+	def somethingelse do
+	end
+
+end	
 
 defmodule Manager do
 	def manage(serverlist) do
 		receive do
 		{:addserver, serverpid} ->
-				 IO.puts"Server process #{inspect self()} is adding #{inspect serverpid} to list"
+				 #IO.puts"Server process #{inspect self()} is adding #{inspect serverpid} to list"
 				 serverlist = serverlist ++ [serverpid] 
 		end
 		IO.puts"#{inspect serverlist} here it is"
